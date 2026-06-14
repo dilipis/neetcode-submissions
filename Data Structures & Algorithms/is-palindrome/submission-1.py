510dc1941a14a4a1ddf3fbfa05ad8f7bb93c34aa
dilipis <26098228+dilipis@users.noreply.github.com>
2026-06-14 17:36:09 -0500

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -0,0 +1,28 @@
+class Solution:
+    import re
+    def isPalindrome(self, s: str) -> bool:
+        
+        # Remove non alphanumeric chars and make lowercase
+        # s = "".join([c.lower() for c in s if c.isalnum()])
+        s = re.sub(r'[^a-zA-Z0-9]','',s).lower()
+
+        fctr = 0
+        bctr = len(s) - 1
+        isPal = True
+
+        print(f"{s=}")
+
+        print(f"{fctr=}\n{bctr=}")
+
+        # Traverse from both sides. If correpding items are same, continue, else break
+        while fctr <= bctr:
+            print(f"{fctr=},{bctr=},{s[fctr]=},{s[bctr]=}")
+            if s[fctr] != s[bctr]:
+                isPal = False
+                break
+            fctr += 1
+            bctr -= 1
+
+        return isPal
+
+        
